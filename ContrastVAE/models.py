24d3dcb21ed8e71c90261367db121363043ff78e
joseph <skdytpq98@naver.com>
2023-05-17 18:59:25 +0900

    f

--- ContrastVAE/models.py
+++ ContrastVAE/models.py
@@ -76,7 +76,7 @@ class ContrastVAE(nn.Module):
         self.temperature = nn.Parameter(torch.zeros(1), requires_grad=True)
 
     def add_position_embedding(self, sequence):
-
+        sequence = sequence.cuda()
         seq_length = sequence.size(1)
         position_ids = torch.arange(seq_length, dtype=torch.long).cuda()
         position_ids = position_ids.unsqueeze(0).expand_as(sequence)
@@ -85,7 +85,7 @@ class ContrastVAE(nn.Module):
         position_embeddings = self.position_embeddings(position_ids)
         position_encoding = self.position_encoding(sequence)
         if self.args.encoding :
-            sequence_emb = item_embeddings + position_encoding + position_embeddings
+            sequence_emb = item_embeddings + position_encoding + position_embeddings.cuda()
         else:
             sequence_emb = item_embeddings + position_embeddings
         sequence_emb = self.LayerNorm(sequence_emb)
